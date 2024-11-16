831ac67f1ab5760cfe49cf9619744620bb27e232
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2024-11-16 10:32:23 -0300

    upload projeto

---
+++ motor-cc-rna.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 16 10:28:50 2024
+
+@author: jacques
+"""
+import pandas as pd
+bancoDB = pd.read_csv('./BD_Jean_98000.csv')
+print(bancoDB.head())
+
+#%%
